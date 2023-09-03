9d19412a7d0cbca197791a410ab182a42e309d0f
pradeep nayak <pradeep@trustcheckr.com>
2023-09-03 09:26:10 +0530

    eslint rules updated

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const http = require("http");
 const app = require("./app");
 
 let httpServer = http.createServer(app);
-let data=httpServer.listen(3001, () => {
+let  data =httpServer.listen(3001, () => {
   console.log("server up and running: ", 3001);
 });
